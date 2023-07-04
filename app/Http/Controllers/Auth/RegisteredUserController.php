b442a81fb8140eef356db9b159ff68db4075d3b5
Odame <kwesi.danquah@daaseng.com>
2023-07-04 21:52:11 +0100

    code refactoring

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -41,7 +41,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'role_id' => RoleEnum::CUSTOMER->value,
+            'role_id' => RoleEnum::CUSTOMER,
         ]);
 
         event(new Registered($user));
