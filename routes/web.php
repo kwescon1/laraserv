d01d3f3b2952fb1d7b37ff24fc33d93533c07fcb
Odame <kwesi.danquah@daaseng.com>
2023-07-04 21:51:24 +0100

    configure route with middleware

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::resource('companies', App\Http\Controllers\CompanyController::class);
+    Route::resource('companies', App\Http\Controllers\CompanyController::class)->middleware('isAdmin');
 });
 
 require __DIR__.'/auth.php';
