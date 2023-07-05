c26517a236fe8faef44b90ccc8957626bf7d15ad
Odame <kwesi.danquah@daaseng.com>
2023-07-05 14:17:53 +0100

    set price mutator and accessor

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -23,5 +24,13 @@ class Activity extends Model
     {
         //activity belongs to many participants
         return $this->belongsToMany(User::class)->withTimestamps();
+    }
+
+    public function price(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value / 100,
+            set: fn ($value) => $value * 100,
+        );
     }
 }
