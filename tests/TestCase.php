230e518dddd8fac07d45440084a9bc87ef34d954
Odame <kwesi.danquah@daaseng.com>
2023-07-05 23:32:14 +0100

    seed only roles

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use Database\Seeders\RoleSeeder;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
@@ -9,4 +10,7 @@ abstract class TestCase extends BaseTestCase
     use CreatesApplication;
 
     protected bool $seed = true;
+
+    // seed only role seeder
+    protected string $seeder = RoleSeeder::class;
 }
