222fa313377f59091544450b56357fa1d3efb75f
Odame <kwesi.danquah@daaseng.com>
2023-07-06 00:53:40 +0100

    add guide activities route

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,7 @@ Route::middleware('auth')->group(function () {
     Route::resource('companies.activities', App\Http\Controllers\CompanyActivityController::class);
 
     Route::get('/activities', [App\Http\Controllers\MyActivityController::class, 'show'])->name('my-activity.show');
+    Route::get('/guides/activities', [App\Http\Controllers\GuideActivityController::class, 'show'])->name('guide-activity.show');
     Route::delete('/activities/{activity}', [App\Http\Controllers\MyActivityController::class, 'destroy'])->name('my-activity.destroy');
 });
 
