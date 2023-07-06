00070e6f61e5fd253b12f7ac6051b2bbff852a58
Odame <kwesi.danquah@daaseng.com>
2023-07-06 01:03:17 +0100

    export activity to pdf

--- app/Http/Controllers/GuideActivityController.php
+++ app/Http/Controllers/GuideActivityController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Activity;
 use App\Models\Enums\RoleEnum;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Response;
 
 class GuideActivityController extends Controller
@@ -15,5 +16,16 @@ class GuideActivityController extends Controller
         $activities = Activity::where('guide_id', auth()->id())->orderBy('start_time')->get();
 
         return view('activities.guide-activities', compact('activities'));
+    }
+
+    public function export(Activity $activity)
+    {
+        abort_if(auth()->user()->role_id !== RoleEnum::GUIDE, Response::HTTP_FORBIDDEN);
+
+        $data = $activity->load(['participants' => function ($query) {
+            $query->orderByPivot('created_at');
+        }]);
+
+        return Pdf::loadView('activities.pdf', ['data' => $data])->download("{$activity->name}.pdf");
     }
 }
